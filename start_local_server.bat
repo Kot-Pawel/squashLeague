e38d80dfa6d446ed9d01b859ab8e6dad226c5e15
Kot-Pawel <109867834+Kot-Pawel@users.noreply.github.com>
2025-08-24 22:25:48 +0200

    Minor changes
    Minor changes

--- start_local_server.bat
+++ start_local_server.bat
@@ -1,4 +1,4 @@
 @echo off
 ECHO Server started at http://localhost:8000
 ECHO Press Ctrl+C to stop the servers
-"C:\Program Files\Git\bin\bash.exe" --login -i -c "cd /c/%DIR_GIT%/squashLeague && python -m http.server 8000"
+"C:\Program Files\Git\bin\bash.exe" --login -i -c "cd /%DIR_GIT%/squashLeague && python -m http.server 8000"
